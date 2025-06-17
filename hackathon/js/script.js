2a9eca471a97e0e44b895378436e4b192561beaf
https://github.com/YooDxE3 <andreox27@gmail.com>
2025-06-17 00:41:59 -0300

    Documentação

--- hackathon/js/script.js
+++
@@ -1,4 +0,0 @@
-  document.querySelector('input[name="cpf"]').addEventListener('input', function(e) {
-    this.value = this.value.replace(/\D/g, '')
-  });
-  
--- hackathon/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-unset($_SESSION['token']);
-unset($_SESSION['usuario']);
-
-header("Location: ./formulario_login.php");
-exit;
